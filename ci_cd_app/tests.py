8fbfcc29f68576254cea09bec541cc4424cbf61f
Ana <anageguchadze22@gmail.com>
2025-05-10 18:13:39 +0400

    Add tests

--- ci_cd_app/tests.py
+++ ci_cd_app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Animal
 
-# Create your tests here.
+class AnimalModelTest(TestCase):
+    def setUp(self):
+        Animal.objects.create(name="Leo", species="Lion", age=5, habitat="Savannah")
+
+    def test_animal_str_method(self):
+        animal = Animal.objects.get(name="Leo")
+        self.assertEqual(str(animal), "Leo (Lion)")
+
+    def test_animal_fields(self):
+        animal = Animal.objects.get(name="Leo")
+        self.assertEqual(animal.age, 5)
+        self.assertEqual(animal.habitat, "Savannah")
